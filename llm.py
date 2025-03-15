5da41e4ee62c664547cd09439af6a34020e2f95a
Atroooo <lucas.compiegne1@gmail.com>
2025-03-15 23:41:56 +0100

    test

--- llm.py
+++ llm.py
@@ -6,6 +6,16 @@ from vllm import LLM, SamplingParams
 load_dotenv()
 HfFolder.save_token(os.getenv("HF_TOKEN"))
 
+
+def print_outputs(outputs):
+    for output in outputs:
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        print(f"Prompt: {prompt!r}")
+        print(f"Generated text: {generated_text!r}")
+    print("-" * 80)
+
+
 model_name = "TheBloke/Mistral-7B-Instruct-v0.2-GPTQ"
 sampling_params = SamplingParams(max_tokens=4096)
 llm = LLM(
@@ -13,16 +23,40 @@ llm = LLM(
     dtype="float16",
 )
 
-# Define the messages for the conversation
-messages = [
-    {
-        "role": "user",
-        "content": "Qui est le meilleur peintre français ? Répondez avec des explications détaillées.",
-    }
+# conversation = [
+#         {
+#             "role": "user",
+#             "content": "Hello"
+#         },
+#         {
+#             "role": "assistant",
+#             "content": "Hello! How can I assist you today?"
+#         },
+#         {
+#             "role": "user",
+#             "content":
+#             "Write an essay about the importance of higher education.",
+#         },
+#         {
+#             "role": "assistant",
+#             "content":
+#             "No worries!",
+#         },
+#         {
+#             "role": "user",
+#             "content":
+#             "What did I ask you to do on the last message ?",
+#         },
+#     ]
+
+# outputs = llm.chat(conversation, sampling_params, use_tqdm=False)
+
+prompts = [
+    "Hello this is a test.",
+    "What did I say on the last message?",
+    "What is the meaning of life?",
 ]
 
-# Generate a response
-res = llm.chat(messages=messages, sampling_params=sampling_params)
 
-# Print the response
-print(res[0].outputs[0].text)
+outputs = llm.generate(prompts, sampling_params, use_tqdm=False)
+print_outputs(outputs)
